d1c1a1549445a6c29cf39029279e6f36c912a321
Jessica Kerr <jessitron@gmail.com>
2017-04-15 13:41:24 -0500

    Let's do this in an editor

--- .atomist/editors/ChangeMemoryRequirements.ts
+++ .atomist/editors/ChangeMemoryRequirements.ts
@@ -1,5 +1,6 @@
 import { EditProject } from '@atomist/rug/operations/ProjectEditor';
 import { Project } from '@atomist/rug/model/Project';
+import { File } from '@atomist/rug/model/File';
 import { Pattern } from '@atomist/rug/operations/RugOperation';
 import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
 
@@ -21,6 +22,25 @@ export class ChangeMemoryRequirements implements EditProject {
     inputParameter: string;
 
     edit(project: Project) {
+        let pxe = project.context.pathExpressionEngine;
+
+        pxe.with<File>(project, "/File()", f => {
+            if (f.name.match(/^80.*deployment.json$/)) {
+                let spec = JSON.parse(f.content);
+                try {
+                    let currentMemoryLimit = spec.spec.template.spec.containers[0].resources.limits.memory;
+                    if (currentMemoryLimit == "1024Mi") {
+                        spec.spec.template.spec.containers[0].resources.limits.memory = "256Mi";
+                    }
+                    let currentMemoryRequest = spec.spec.template.spec.containers[0].resources.requests.memory;
+                    if (currentMemoryLimit == "768Mi") {
+                        spec.spec.template.spec.containers[0].resources.requests.memory = "128Mi";
+                    }
+                } catch (e) {
+                    console.log("looks like we got the structure wrong: " + e);
+                }
+            }
+        })
         project.addFile("hello.txt", "Hello, World!\n" + this.inputParameter + "\n");
     }
 }
